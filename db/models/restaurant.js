f723e2757f34e734e69988e120be8b3ada60025a
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-20 13:47:33 +0300

    Restaurant Schema

---
+++ db/models/restaurant.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const restaurant = new mongoose.Schema({
+  Name: { type: String, required: true },
+  Category: { type: String, required: true },
+  Picture: { type: String, required: true },
+  DeliveryPrice: { type: Number },
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Foodmenu" },
+});
+
+module.exports = mongoose.model("Restaurant", restaurant);
