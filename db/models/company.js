d6899d36842516d1992e8ad189544acea5f2d018
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-20 13:43:26 +0300

    Company Schema

---
+++ db/models/company.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const company = new mongoose.Schema({
+  Name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  passwordCode: { type: String },
+  activeCode: { type: String },
+  active: { type: Boolean, default: false },
+  avatar: { type: String, default: "http://norapc.org/wp-content/uploads/2015/07/avatar-blank.png" },
+  Restaurant: [{ type: mongoose.Schema.Types.ObjectId, ref: "Restaurant" }],
+  Offers: { type: mongoose.Schema.Types.ObjectId, ref: "Offers" },
+  location: {type: Location},
+});
+
+module.exports = mongoose.model("Company", company);
