ab2b01b319f97923ebddd673ad05ec73c7f99e6c
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-20 16:38:04 +0300

    Add new restaurant  Done

--- routers/controllers/restaurant.js
+++ routers/controllers/restaurant.js
@@ -1,29 +1,25 @@
 const restaurantModel = require("./../../db/models/restaurant");
 
 const createRestaurant = (req, res) => {
-    console.log(req.token);
-    const { Name, Category, Picture, DeliveryPrice, Menu } = req.body;
-    const newRestaurant = new restaurantModel({
-        Name,
-        Picture,
-      Category,
-      DeliveryPrice,
-      Menu
+  console.log(req.token);
+  const { Name, Category, Picture, DeliveryPrice, Menu } = req.body;
+  const newRestaurant = new restaurantModel({
+    Name,
+    Picture,
+    Category,
+    DeliveryPrice,
+    Menu,
+  });
+  newRestaurant
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
     });
-    newRestaurant
-      .save()
-      .then((result) => {
-        res.status(201).json(result);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
-  };
+};
 
-
-
-  module.exports = {
-    createRestaurant
-  };
-
-  
+module.exports = {
+  createRestaurant,
+};
