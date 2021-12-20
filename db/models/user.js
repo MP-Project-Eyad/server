80cd1f17bfeaca949c623aad08cfa617064778fd
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-20 13:34:51 +0300

    Role Schema

---
+++ db/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const user = new mongoose.Schema({
+  userName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  passwordCode: { type: String },
+  activeCode: { type: String },
+  active: { type: Boolean, default: false },
+  avatar: { type: String, default: "avatar" },
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role", default: "" },
+  location: {type: Location},
+});
+
+// module.exports = mongoose.model("User", user);
+
