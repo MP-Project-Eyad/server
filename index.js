156207d9a1391d0c37b1ce0e8f4d312f1d253b34
Eyad911 <Eyad.Abdulhamid.elq.js@tuwaiq.edu.sa>
2021-12-20 13:11:56 +0300

    set up Server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+// require("./db");
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+
+// const roleRouter = require('./routers/routes/role');
+// app.use(roleRouter);
+
+// const userRouter = require('./routers/routes/user');
+// app.use(userRouter);
+
+// const postRouter = require('./routers/routes/post');
+// app.use(postRouter);
+
+// const commentRouter = require('./routers/routes/comment');
+// app.use(commentRouter);
+
+// const likeRouter = require('./routers/routes/like');
+// app.use(likeRouter);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`SERVER RUN ON ${PORT}`);
+});
